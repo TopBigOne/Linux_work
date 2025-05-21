f6882775218c6f33c967bc8e0f30c351eede3a61
dev_mu <devMu@gmail.com>
2025-05-21 19:16:13 +0800

    [BIO][demo-2] format code.

--- linux_io/BIO/Demo2/main.c
+++ linux_io/BIO/Demo2/main.c
@@ -3,28 +3,29 @@
 #include <netinet/in.h>
 
 void test();
-void test(){
+
+void test() {
     // TCP Socket
-    int sock_fd = socket(AF_INET,SOCK_STREAM,0);
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {
             .sin_family = AF_INET,
             .sin_port = htons(8080), //监听8080 端口
             .sin_addr.s_addr = INADDR_ANY// 接受任意ip连接
-                        };
+    };
 
     bind(sock_fd, (const struct sockaddr *) &addr, sizeof addr);
-    listen(sock_fd,5);// 允许5个连接排队
+    listen(sock_fd, 5);// 允许5个连接排队
     puts("serer start listen.....");
     //  这个地方有阻塞，直到有客户端连接
-    int client_fd = accept(sock_fd,NULL,NULL);
+    int client_fd = accept(sock_fd, NULL, NULL);
 
     puts("Client Connected.");
     char buf[100];
-    ssize_t  n =recv(client_fd,buf, sizeof(buf),0);
-    printf("Receiveed : %.*s \n",(int)n ,buf);
+    ssize_t n = recv(client_fd, buf, sizeof(buf), 0);
+    printf("Receiveed : %.*s \n", (int) n, buf);
 }
 
 int main(void) {
-   test();
+    test();
     return 0;
 }
